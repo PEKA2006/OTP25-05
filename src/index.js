0f90bea5d96122d6ccf96918d484439ec5d4c05f
PEKA2006 <143948905+PEKA2006@users.noreply.github.com>
2025-05-25 22:16:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Only works after installing bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-    <BrowserRouter>
+  <BrowserRouter basename="/OTP25-05">
     <App />
-    </BrowserRouter>
-  
+  </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
